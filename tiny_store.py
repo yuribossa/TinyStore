8f90b842ec2c1118aba5e00e5088382f5403c87c
yuribossa <drop2nine@gmail.com>
2009-12-16 20:14:07 +0900

    add delete() and check()

--- tiny_store.py
+++ tiny_store.py
@@ -3,11 +3,13 @@
 # -- TinyStore --
 # TinyStore is the easy way of storing data into Datastore on Google App Engine.
 # The use of TinyStore is very easy.
-# TinyStore have only two functions, write and read.
-# Please report to me about any bug reports or questions.
-# TinyStore is Released into the Public Domain.
+# TinyStore have three functions, write(), read(), check() and delete().
+# All data-types supported by GAE can store TinyStore.
+# TinyStore is released into the Public Domain.
 # 
+
 # Author: Yuribossa
+# Hosted: http://github.com/yuribossa/TinyStore
 # Homepage: http://yuribossa.appspot.com/
 # Mail: yuribossa@gmail.com
 # 
@@ -18,11 +20,12 @@
 # x = TinyStore()
 # x.write('dog_name', 'alex')
 # y = x.read('dog_name')
+# if x.check('dog_name'):
+#     x.delete('dog_name')
 # 
 
 
 from google.appengine.ext import db
-import types
 
 class TinyStoreModel(db.Expando):
     key_ = db.StringProperty()
@@ -37,10 +40,10 @@ class TinyStore():
             if isinstance(key, (str, unicode)):
                 query = TinyStoreModel(key_=key, value=value)
             else:
-                return 0
+                return False
         
         query.put()
-        return 1
+        return True
     
     def read(self, key):
         query = db.Query(TinyStoreModel).filter('key_ =', key)
@@ -48,3 +51,19 @@ class TinyStore():
             query = query.get()
             return query.value
         return None
+    
+    def check(self, key):
+        query = db.Query(TinyStoreModel).filter('key_ =', key)
+        if query.count():
+            return True
+        else:
+            return False
+    
+    def delete(self, key):
+        query = db.Query(TinyStoreModel).filter('key_ =', key)
+        if query.count():
+            query = query.get()
+            query.delete()
+            return True
+        else:
+            return False
